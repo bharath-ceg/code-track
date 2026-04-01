8a4eba0f46d7520b4b5f15f1f53445718fb3cf73
Bharath <bharath151206@gmail.com>
2026-04-01 21:42:31 +0530

    Fix backend logic to support Admin registration

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -2,12 +2,12 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 exports.registerUser = async (userData) => {
-  const { name, registerNumber, department, email, password } = userData;
+  const { name, registerNumber, department, email, password, role } = userData;
   const userExists = await User.findOne({ $or: [{ email }, { registerNumber }] });
   if (userExists) {
     throw new Error('User already exists');
   }
-  const user = await User.create({ name, registerNumber, department, email, password });
+  const user = await User.create({ name, registerNumber, department, email, password, role });
   return user;
 };
 
